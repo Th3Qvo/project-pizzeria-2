01a9d890b0851d1aad5bdb7c51e9c5c0be26e529
Mateusz Fabisiak <mateusz.fabisiak.87@gmail.com>
2021-11-19 17:05:01 +0100

    ADD initOrderForm and processOrder-in progress

--- src/js/script.js
+++ src/js/script.js
@@ -5,19 +5,19 @@
 
   const select = {
     templateOf: {
-      menuProduct: '#template-menu-product',
+      menuProduct: '#template-menu-product', // templatka do Handlebars
     },
     containerOf: {
       menu: '#product-list',
       cart: '#cart',
     },
     all: {
-      menuProducts: '#product-list > .product',
-      menuProductsActive: '#product-list > .product.active',
+      menuProducts: '#product-list > .product', // pojedynczy produkt w templatce
+      menuProductsActive: '#product-list > .product.active', // pojedynczy produkt w templatce - AKTYWNY!
       formInputs: 'input, select',
     },
     menuProduct: {
-      clickable: '.product__header',
+      clickable: '.product__header', // nagłówek produktu, który po kliknięciu ma sie rozwinąć
       form: '.product__order',
       priceElem: '.product__total-price .price',
       imageWrapper: '.product__images',
@@ -35,7 +35,7 @@
 
   const classNames = {
     menuProduct: {
-      wrapperActive: 'active',
+      wrapperActive: 'active', // target aktywnej klasy produktu
       imageVisible: 'active',
     },
   };
@@ -49,7 +49,7 @@
   };
 
   const templates = {
-    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML),
+    menuProduct: Handlebars.compile(document.querySelector(select.templateOf.menuProduct).innerHTML), // skompilowana templatka
   };
 
   class Product {
@@ -60,7 +60,10 @@
       thisProduct.data = data;
 
       thisProduct.renderInMenu();
+      thisProduct.getElements();
       thisProduct.initAccordion();
+      thisProduct.initOrderForm();
+      thisProduct.processOrder();
     }
 
     renderInMenu(){
@@ -76,13 +79,22 @@
       menuContainer.appendChild(thisProduct.element); // wstawiamy utoworzy element DOM z kodem HTML do odpowiedniego <div></div> w kodzie HTML
     }
 
+    getElements(){
+      const thisProduct = this;
+
+      // dodajemy referencje do elementów HTML jako właściwości do naszego obiektu
+      thisProduct.accordionTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); // referencja do nagłówka (clickable trigger)
+      thisProduct.form = thisProduct.element.querySelector(select.menuProduct.form); // referencja do formularza produktu
+      thisProduct.formInputs = thisProduct.form.querySelectorAll(select.all.formInputs); // referencja do kontrolek w formularzu
+      thisProduct.cartButton = thisProduct.element.querySelector(select.menuProduct.cartButton); // referencja do buttona
+      thisProduct.priceElem = thisProduct.element.querySelector(select.menuProduct.priceElem); // referencja do kontenerka z ceną produktu
+    }
+
     initAccordion(){
       const thisProduct = this;
 
-      // find the clickable trigger
-      const clickableTrigger = thisProduct.element.querySelector(select.menuProduct.clickable); // targetuje nagłówek
       // START add eventListener to clickable trigger
-      clickableTrigger.addEventListener('click', function(event){
+      thisProduct.accordionTrigger.addEventListener('click', function(event){ // korzysta z getElements, żeby wybrać target klikania
         // prevent default action
         event.preventDefault();
         // find active product (with class active)
@@ -94,6 +106,32 @@
         // toggle class active to active element
         thisProduct.element.classList.toggle(classNames.menuProduct.wrapperActive); // przełącza klase active to aktualnie wybranego elementu
       });
+    }
+
+    initOrderForm(){
+      const thisProduct = this;
+
+      thisProduct.form.addEventListener('submit', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+
+      for(let input of thisProduct.formInputs){
+        input.addEventListener('change', function(){
+          thisProduct.processOrder();
+        });
+      }
+
+      thisProduct.cartButton.addEventListener('click', function(event){
+        event.preventDefault();
+        thisProduct.processOrder();
+      });
+    }
+
+    processOrder(){
+      const thisProduct = this;
+
+      console.log(thisProduct);
     }
   }
 
